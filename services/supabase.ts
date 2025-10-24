c3f94764f41a318c6b25c9673490c15ffd0ff264
Melqui <melqui.sodre15@gmail.com>
2025-10-23 22:06:00 -0300

    chore: env supabase

--- services/supabase.ts
+++ services/supabase.ts
@@ -1,13 +1,16 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 let supabaseInstance: any = null;
 let connectionError: string | null = null;
 
+console.log("supabaseUrl", supabaseUrl)
+console.log("supabaseKey", supabaseKey)
+
 if (!supabaseUrl || !supabaseKey) {
-  connectionError = "As variáveis de ambiente NEXT_PUBLIC_SUPABASE_URL e NEXT_PUBLIC_SUPABASE_ANON_KEY não foram definidas.";
+  connectionError = "As variáveis de ambiente SUPABASE_URL e NEXT_PUBLIC_SUPABASE_ANON_KEY não foram definidas.";
   console.error(connectionError);
 } else {
   try {
